d853a4cffd935a5b27d5a5646f9fb9af786b435f
thaparojanthapa1-eng <thaparojanthapa1@gmail.com>
2026-04-16 19:21:12 +0545

    Initialised database

--- website/__init__.py
+++ website/__init__.py
@@ -1,7 +1,19 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+Db=SQLAlchemy()
+DB_NAME="database.db"
 
 def create_app():
     app=Flask(__name__)
     app.config['SECRET_KEY']="Hello World"
+    app.config["SQLALCHEMY_DATABASE_URI"]=f"sqlite:///{DB_NAME}"
+    Db.init_app(app)
+
+    from .auth import auth
+    from .views import views
+
+    app.register_blueprint(auth, url_prefix="/")
+    app.register_blueprint(views, url_prefix="/")
 
     return app
