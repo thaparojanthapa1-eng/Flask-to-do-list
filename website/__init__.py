246aa42ae585f6811920867433fb3dcf493189f3
thaparojanthapa1-eng <thaparojanthapa1@gmail.com>
2026-04-16 19:48:08 +0545

    Added a login manager

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_login import LoginManager
 
 Db=SQLAlchemy()
 DB_NAME="database.db"
@@ -16,6 +17,18 @@ def create_app():
 
     app.register_blueprint(auth, url_prefix="/")
     app.register_blueprint(views, url_prefix="/")
+
+    from .model import User
+
+    create_database(app)
+
+    login_manager=LoginManager()
+    login_manager.login_view="auth.login"
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     return app
 
