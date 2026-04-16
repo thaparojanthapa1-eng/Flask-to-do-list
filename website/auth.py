79f1f82a840271db5be73c4bae3fbcfc79f35bd8
thaparojanthapa1-eng <thaparojanthapa1@gmail.com>
2026-04-16 19:37:01 +0545

    Import Db in auth and added logout feature

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, render_template, url_for, redirect, request, flash
+from . import Db
+from flask_login import login_user, logout_user, login_required
 
 auth=Blueprint("auth", __name__)
 
@@ -8,6 +10,8 @@ def login():
         email=request.form.get("email")
         password=request.form.get("password")
 
+        return redirect(url_for("signup"))
+
     return render_template("login.html")
 
 @auth.route("/signup", methods=["POST", "GET"])
@@ -15,5 +19,7 @@ def signup():
     return render_template("signup.html")
 
 @auth.route("/logout")
+@login_required
 def logout():
-    pass
+    logout_user()
+    return redirect(url_for("login"))
