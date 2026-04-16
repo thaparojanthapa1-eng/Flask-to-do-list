d08219559b400010d846f912560ff284c33569fc
thaparojanthapa1-eng <thaparojanthapa1@gmail.com>
2026-04-16 19:20:41 +0545

    Minor changes

--- website/auth.py
+++ website/auth.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, url_for, redirect, request, flash
 
 auth=Blueprint("auth", __name__)
 
-@auth.route("login", methods=["POST", "GET"])
+@auth.route("/login", methods=["POST", "GET"])
 def login():
     if request.method=="POST":
         email=request.form.get("email")
@@ -10,10 +10,10 @@ def login():
 
     return render_template("login.html")
 
-@auth.route("signup")
+@auth.route("/signup", methods=["POST", "GET"])
 def signup():
-    pass
+    return render_template("signup.html")
 
-@auth.route("logout")
+@auth.route("/logout")
 def logout():
     pass
