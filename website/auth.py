41c7e8f203c167983065f52bc4ddace82a1afb74
thaparojanthapa1-eng <thaparojanthapa1@gmail.com>
2026-04-20 17:41:02 +0545

    add login validation for user existence and password check

--- website/auth.py
+++ website/auth.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, url_for, redirect, request, flash
 from . import Db
 from flask_login import login_user, logout_user, login_required
+from .model import User
 
 auth=Blueprint("auth", __name__)
 
@@ -10,7 +11,16 @@ def login():
         email=request.form.get("email")
         password=request.form.get("password")
 
-        return redirect(url_for("signup"))
+        user=User.query.filter_by(email=email).first()
+        if user:
+            if password==user.password:
+                flash("Logged in!", category="success")
+                login_user(user, remember=True)
+                return redirect(url_for("views.home"))
+            else:
+                flash("Password is incorrect", category="error")
+        else:
+            flash("User doesn't exist", category="error")
 
     return render_template("login.html")
 
