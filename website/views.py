95788ba7e1bdc83d80e43799f97cdccb6bfcab3c
thaparojanthapa1-eng <thaparojanthapa1@gmail.com>
2026-04-20 18:16:52 +0545

    Add login protection and task creation

--- website/views.py
+++ website/views.py
@@ -1,8 +1,29 @@
 from flask import Blueprint, redirect, render_template, request, flash, url_for
+from flask_login import login_required, current_user
+from . import Db
+from .model import User, Task
 
 views=Blueprint("views", __name__)
 
 @views.route("/")
 @views.route("/home")
+@login_required
 def home():
-    return render_template("home.html")
+    tasks=Task.query.all()
+    return render_template("home.html", user=current_user, tasks=tasks)
+
+@views.route("/addtask", methods=["POST", "GET"])
+def add_task():
+    if request.method=="POST":
+        text=request.form.get("text")
+
+        if not text:
+            flash("Post can't be empty", category="error")
+        else:
+            task=Task(text=text, author_id=current_user.id)
+            Db.session.add(task)
+            Db.session.commit()
+            flash("Task added", category="success")
+            return(redirect(url_for("views.home")))
+        
+    return render_template("addtask.html")
